789c1977fd56a53bf6e28e21ccc9f7d78301f323
chaoyue <beberoy@sina.com>
2018-01-16 21:40:46 +0800

    python hello world

--- chaoyue/helloWorld.py
+++ chaoyue/helloWorld.py
@@ -1,2 +1,19 @@
+# coding=utf-8
+# print两种写法都可以
 print "hello world"
 print("hello chaoyue")
+# if-else 的写法 使用缩进来确定作用域
+if True:
+    print("this is true")
+else:
+    print("this is false")
+
+    '''
+    字符串类型使用以下三种定义都可以
+    可以换行
+    '''
+word = 'this is a word'
+word2 = "this is a word2"
+word3 = """this is a word3
+233333"""
+print word + word2 + word3
